bb24b2ea5cccdc4d6ce5f79ed15794b1ecb6a335
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-01-14 23:29:16 +0100

    feat: Ajout react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import './App.css';
 import MovieList from "./components/MovieList/MovieList.jsx";
+import {Route, Routes} from "react-router";
+import MovieDetail from "./components/MovieDetail/MovieDetail.jsx";
 
 function App() {
-
     return (
-        <>
-            <MovieList/>
-        </>
-    )
+        <div>
+            <Routes>
+                <Route path="/" element={<MovieList/>}/>
+                <Route path="/movie/:id" element={<MovieDetail/>}/>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import MovieDetail from './components/MovieDetail/MovieDetail.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/movie/:id" element={<MovieDetail/>}/>
+        </Routes>
+    </BrowserRouter>,
 )
