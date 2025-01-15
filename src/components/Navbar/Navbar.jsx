2f3b914607d0d1031edd6f502ca4a9a2232edc0d
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-01-15 11:19:41 +0100

    fix: Passage du css de Navbar en module

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import './Navbar.css';
+import './Navbar.module.css';
 import {Link} from "react-router";
 import {useWishlist} from '../../context/WishlistContext';
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     const {wishlist} = useWishlist();
     console.log(wishlist);
 
     return (
-        <nav className="navbar">
+        <nav className={styles.navbar}>
             <Link to="/"><h1>CinéClub</h1></Link>
             <ul>
                 <li><Link to="/">Accueil</Link></li>
