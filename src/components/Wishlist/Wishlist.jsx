f12a6e3b03814f00ea4c3f505292548939fe3115
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-01-15 11:22:09 +0100

    fix: Passage du css de Whishlist en module

--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -1,15 +1,16 @@
+import styles from './Wishlist.module.css';
 import {useWishlist} from '../../context/WishlistContext';
-import './Wishlist.css';
 
 const Wishlist = () => {
     const {wishlist, removeFromWishlist} = useWishlist();
     return (
-        <div className="wishlist">
+        <div className={styles["wishlist"]}>
             <h2>Wishlist</h2>
             <ul>
                 {wishlist.map(movie => (
                     <li key={movie.id}>
-                        <img className="movie-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        <img className={styles["movie-image"]}
+                             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                              alt={movie.title}/>
                         <div>
                             <h3>{movie.title}</h3>
@@ -20,6 +21,6 @@ const Wishlist = () => {
             </ul>
         </div>
     );
-}
+};
 
 export default Wishlist;
