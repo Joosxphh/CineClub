e1ba238ee61a43f3ded30d5b2d5042dffca9b3b0
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-01-15 11:37:10 +0100

    style: Ajustement css

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,14 +3,20 @@ import {Link} from "react-router";
 
 const MovieCard = ({movie}) => {
     return (
-        <div className={styles["movie-card"]}>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-            <h3>{movie.title}</h3>
-            <p>{movie.vote_average} ⭐</p>
-            <div className={styles["button-container"]}>
-                <Link to={`/movie/${movie.id}`} className={styles.button}>Voir les détails</Link>
+        <Link to={`/movie/${movie.id}`} className={styles.card}>
+            <div className={styles["movie-card"]}>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+                <div className={styles["rating-circle"]}>
+                    <span>{movie.vote_average.toFixed(1)} ⭐</span>
+                </div>
+                <h3>{movie.title}</h3>
+                <div className={styles["button-container"]}>
+                    <span className={styles.button}>
+                        Voir les détails
+                    </span>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
