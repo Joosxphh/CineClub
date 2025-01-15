f310c615578a376463d3872ef639131db0706965
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-01-15 11:15:04 +0100

    fix: Passage du css de MovieCard en module

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,18 +1,17 @@
-import './MovieCard.css';
+import styles from './MovieCard.module.css';
 import {Link} from "react-router";
 
 const MovieCard = ({movie}) => {
     return (
-
-        <div className="movie-card">
+        <div className={styles["movie-card"]}>
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <h3>{movie.title}</h3>
             <p>{movie.vote_average} ⭐</p>
-            <div className="button-container">
-                <Link to={`/movie/${movie.id}`} className="button">Voir les détails</Link>
+            <div className={styles["button-container"]}>
+                <Link to={`/movie/${movie.id}`} className={styles.button}>Voir les détails</Link>
             </div>
         </div>
     );
-}
+};
 
 export default MovieCard;
